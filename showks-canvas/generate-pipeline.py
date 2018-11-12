3a201fd5a867e67e8297b68f549f060e91143f92
jyoshise <yoshise@gmail.com>
2018-11-12 16:25:28 +0900

    moidry generate-pipeline.py

--- showks-canvas/generate-pipeline.py
+++ showks-canvas/generate-pipeline.py
@@ -4,7 +4,7 @@ import argparse
 
 def get_args():
     parser = argparse.ArgumentParser()
-  
+
     parser.add_argument("id", help="id", type=str)
     parser.add_argument("template", help="template file path", type=str)
     parser.add_argument("outfile", help="output json file path", type=str)
@@ -19,12 +19,19 @@ def main():
     with open(args.template) as f:
         df = json.load(f)
 
-    df['expectedArtifacts'][0]['matchArtifact']['id'] = 'showks-canvas-' + args.id + '-manifest-github'
-    # 以下同様に並べていく
+    APPNAME = 'showks-canvas' + args.id
+
+    df['application'] = APPNAME
+    df['expectedArtifacts'][0]['defaultArtifact']['id'] = APPNAME + '-defaultArtifact'
+    df['expectedArtifacts'][0]['id'] = APPNAME + '-manifest'
+    df['expectedArtifacts'][0]['matchArtifact']['id'] = APPNAME + '-manifest-github'
+    df['id'] = APPNAME + '-pipeline'
+    df['stages'][0]['manifestArtifactId'] = APPNAME + '-manifest'
+    df['triggers'][0]['expectedArtifactIds'][0] = APPNAME + '-manifest'
+    df['triggers'][0]['slug'] = APPNAME  #GitHub Repo Name
 
     with open(args.outfile, 'w') as of:
-        json.dump(df, of, indent=4)
+        json.dump(df, of, indent=2)
 
 if __name__ == '__main__':
     main()
-
